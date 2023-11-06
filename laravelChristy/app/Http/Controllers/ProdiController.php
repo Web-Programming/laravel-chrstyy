ca0f69565beba17c351fcc1ddefa8a84b2f984d0
Christy <118579935+chrstyy@users.noreply.github.com>
2023-11-06 13:23:57 +0700

    eloquent relationship

--- laravelChristy/app/Http/Controllers/ProdiController.php
+++ laravelChristy/app/Http/Controllers/ProdiController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class prodiController extends Controller
 {
     //
     public function index(){
         $kampus = "Universitas Multi Data Palembang";
+        $result = DB::select('select mahasiswas.*, prodis.nama as nama_prodi from prodis, mahasiswas where prodis.id = mahasiswas.prodi_id');
         return view('prodi.index')->with('kampus', $kampus);
     }
 }
